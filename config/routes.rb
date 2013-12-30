411db656c1b0d7584c403b717010ddf0f2f97e7d
Gordon Zheng <technix1@gmail.com>
2013-12-30 15:11:45 -0500

    rename send action because "send" is reserved

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 DwollaSampleRailsApp::Application.routes.draw do
   root to: 'oauth#begin'
 
-  get "transactions/send"
+  post "transactions/send_funds"
 
   get "oauth/begin"
   get "oauth/return"
-  
+
 end

3cace446c1bcdaaf8de5fab6131753886c26147c
Gordon Zheng <technix1@gmail.com>
2013-12-30 15:11:59 -0500

    implement trivial send demo

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,4 +1,11 @@
 class TransactionsController < ApplicationController
-  def send
+  def send_funds
+  	Dwolla::token = session[:token]
+  	@transaction_id = Dwolla::Transactions.send({
+  		:destinationId => '812-713-9234',  # Reflector Account ID
+  		:destinationType => 'Dwolla',  
+  		:amount => params[:amount],
+  		:pin => params[:pin], 
+  		:notes => 'Yay, money!'})
   end
 end

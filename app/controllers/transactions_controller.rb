04385232704c283c631f43e6fd411aef946a4109
Gordon Zheng <technix1@gmail.com>
2014-01-02 18:31:01 -0500

    remove optional parameters to make it even SIMPLER!

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,9 +3,7 @@ class TransactionsController < ApplicationController
   	Dwolla::token = session[:token]
   	@transaction_id = Dwolla::Transactions.send({
   		:destinationId => '812-713-9234',  # Reflector Account ID
-  		:destinationType => 'Dwolla',  
   		:amount => params[:amount],
-  		:pin => params[:pin], 
-  		:notes => 'Yay, money!'})
+  		:pin => params[:pin]})
   end
 end
